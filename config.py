3de53ffddaa7f02f48f8732d07abb45a3c1dde7a
Mi_ZR <zhourui8@xiaomi.com>
2024-08-14 17:27:01 +0800

    修改bug

--- config.py
+++ config.py
@@ -1,23 +1,36 @@
 import torch
+from utils.mkdir_utils import *
+
+
+def get_now():
+    # 获取当前日期
+    today = datetime.today()
+
+    # 格式化为“年-月-日”格式
+    formatted_date = today.strftime("%Y-%m-%d")
+    return formatted_date
 
 
 class Config:
     # 输入特征数 包含action
-    input_size = 10
+    input_size = 12
     LR = 0.005
     batch_size = 64
     hidden_size = 50
-    epochs = 100
-    training_data_path = "./data/train_year_731.pkl"
-    save_path = "./result/models/year_731"
-    save_test_path = "./result/img/test/year_731"
+    epochs = 300
+    training_data_path = "./data/train_year_814.pkl"
+
+    save_path = mkdir("./result/models")
+
+    save_test_path = "./result/img/test/year_814"
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model_name = 'fixed'
     # 状态数
-    num_features = 9
+    num_features = 11
     # 测试参数
-    test_data_path = './data/test_year_731.pkl'
-    model_path = f'./result/models/{model_name}_best.pth'
+    test_data_path = './data/test_year_814.pkl'
+
+    model_path = f'{save_path}/{model_name}_best.pth'
 
     # --------------------------------------------------------------------------------------
     # dqn参数
@@ -25,29 +38,29 @@ class Config:
     epsilon_min = 0.01
     epsilon_decay = 0.99
     gamma = 0.99
-    dqn_lr = 0.01
-    n_states = 8
-    n_actions = 4
+    dqn_lr = 0.5
+    n_states = 11
+    n_actions = 5
     TRARGE_REPLACE_INTER = 10
-    memory_capacity = 5000
-    dqn_epochs = 2000
-    sampling_size = 10
-    episode_length = 10
-    dqn_batch_size = 100
+    memory_capacity = 20000
+    dqn_epochs = 3000
+    sampling_size = 30
+    episode_length = 24
+    dqn_batch_size = 500
     # strategy
     strategy_list = ["ucb", "boltzmann", "entropy", "noise", "epsilon"]
     strategy = strategy_list[4]
 
     # 消费行为模拟器
-    simulator_model = f"./result/models/fixed_best.pth"
+    simulator_model = model_path
 
     # 结果图保存
     save_img_path = f"./result/img/train_dqn"
 
     # 数据地址
-    data_path = f"./data/DQN_data_new_723.csv"
+    data_path = f"./data/DQN_data_year_814.csv"
 
     # 加载模拟器
-    simulator = torch.load(simulator_model, map_location=torch.device('cpu'))
+    # simulator = torch.load(simulator_model, map_location=torch.device('cpu'))
 
 
